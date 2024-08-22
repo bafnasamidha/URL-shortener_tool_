749f9a78d50324cbdd3d55623db9ad64a62f0025
sam <bafnasamidha@gmail.com>
2024-08-22 23:45:56 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 const URL = require("./models/url's.js");
 
 const { connectMongoDb } = require("./connection.js");
 
 const URLRoute = require("./routes/url's.js");
-
 const staticRoute = require("./routes/staticRouter.js");
+const userRoute=require("./routes/user.js");
 
 connectMongoDb("mongodb://127.0.0.1:27017/short-url");
 
@@ -15,6 +16,8 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/user",userRoute);
 
 app.use("/url", URLRoute);
 
